ab129930efdce8a55dfd6b39ff268cee9686eb8e
Ryan-Dunne <ryanjd01@hotmail.com>
2026-04-22 12:11:39 +0100

    Keeping github file for showcase

---
+++ TerraLockCLI/gh-output-1776854832.tf
@@ -0,0 +1,201 @@
+
+# source: Terraform/AnotherFile.tf
+data "aws_ami" "OtherUbuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_vpc" "OtherMain" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "OtherIgw" {
+  vpc_id = aws_vpc.OtherMain.id
+}
+
+resource "aws_instance" "Other_app_server" {
+  ami           = data.aws_ami.OtherUbuntu.id
+  instance_type = "t3.micro"
+
+  subnet_id              = aws_subnet.OtherPublic.id
+  vpc_security_group_ids = [aws_security_group.Other_app_sg.id]
+
+  tags = {
+    Name = "Other-app-server"
+  }
+}
+
+resource "aws_subnet" "OtherPublic" {
+  vpc_id                  = aws_vpc.OtherMain.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "OtherPublic-subnet"
+  }
+}
+
+resource "aws_route_table" "OtherPublic" {
+  vpc_id = aws_vpc.OtherMain.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.OtherIgw.id
+  }
+}
+
+resource "aws_route_table_association" "OtherPublic_assoc" {
+  subnet_id      = aws_subnet.OtherPublic.id
+  route_table_id = aws_route_table.OtherPublic.id
+}
+
+resource "aws_security_group" "Other_app_sg" {
+  name        = "app-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.OtherMain.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Other-app-sg"
+  }
+}
+
+
+
+
+
+
+
+# source: Terraform/main.tf
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "app-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # consider restricting this
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-sg"
+  }
+}
+
+
+
+
+
+
+
+# source: Terraform/terraform.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
